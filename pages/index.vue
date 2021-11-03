c82a2161bec396afed67191badbb6462204f2bcc
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-03 01:49:28 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-screen w-screen bg-black p-8 flex items-center flex-col">
     <h1 class="text-white text-center mb-4 font-bold">Nuxt Image</h1>
     <nuxt-img
-      src="/8.png"
+      src="/gallery-bg.jpg"
       alt="Image"
       sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"/>
   </div>
